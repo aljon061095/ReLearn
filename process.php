ef23181694630b79f52acb0953d3191f2a9b1bed
Aljon Quiambao <aljon.quiambao@mortgagechoice.com.au>
2022-06-10 15:49:24 +0800

    Update process method

--- process.php
+++ process.php
@@ -2,16 +2,26 @@
 <?php require_once('config.php') ?>
 
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    $rest_json = file_get_contents("php://input");
+    $_POST = json_decode($rest_json, true);
+
+    // print_r($_POST['nickname']);
+?>
+<?php
     if (isset($_POST)) {
         $nickname = $_POST['nickname'];
         $email = $_POST['email'];
         $age = $_POST['age'];
         $gender = $_POST['gender'];
         $grade = $_POST['grade'];
-        $referral_code = $_POST['referral_code'];
+        $referral_code = $_POST['referral'];
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-       
         $query = "INSERT INTO users(nickname, email, age, gender, grade, referral_code, password) 
             VALUES ('$nickname', '$email', '$age', '$gender', '$grade', '$referral_code', '$password')";
             
