51140b6191fb15eddf9d5093977be93c70a7cdd3
Aljon Quiambao <aljon.quiambao@mortgagechoice.com.au>
2022-06-14 13:48:42 +0800

    Update mail and config file

--- config.php
+++ config.php
@@ -2,15 +2,18 @@
     $connect = new PDO("mysql:host=localhost; dbname=relearn", "root", "");
 
     class Database {
-        private $host = "localhost";
+        //This config is intented for our actual database
+        private $host = "13.213.60.83";
+        private $port = 3306;
         private $database_name = "relearn";
-        private $username = "root";
-        private $password = "";
+        private $username = "relearnadmin";
+        private $password = "gofleet0088";
+
         public $conn;
         public function getConnection(){
             $this->conn = null;
             try{
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=" . $this->host .";port=" . $this->port . ";dbname=" . $this->database_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
             }catch(PDOException $exception){
                 echo "Database could not be connected: " . $exception->getMessage();
--- mail.php
+++ mail.php
@@ -12,8 +12,8 @@
     $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
     $mail->SMTPAuth = true;               // Enable SMTP authentication 
     $mail->Username = 'aquiambao061095@gmail.com';   // SMTP username 
-    $mail->Password = 'Aljon061095!';   // SMTP password 
-    $mail->SMTPSecure = 'tsl';            // Enable TLS encryption, `ssl` also accepted 
+    $mail->Password = 'jmrttbcrjqoohjqh';   // SMTP password 
+    $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
     $mail->Port = 587;                    // TCP port to connect to 
     
     // Sender info 
@@ -21,6 +21,7 @@
     
     // Add a recipient 
     $mail->addAddress($recipient_email); 
+    //$mail->addAddress('aljonquiambao.mcc@gmail.com'); 
     
     // Set email format to HTML 
     $mail->isHTML(true); 
@@ -29,7 +30,7 @@
     $mail->Subject = 'Welcome onBoarding ReLearn'; 
     
     // Mail body content 
-    $bodyContent = '<div style="border: 2px solid blue; border-radius: 5px;width: 25%;
+    $bodyContent = '<div style="border: 2px solid blue; border-radius: 5px;width: 50%;
     text-align: center;"><h2>Welcome onBoarding ReLearn!</h2><p>Join our referral program kindly share your referral code to others!</p>'; 
     $bodyContent .= '<h3><b>ST'. $referral_code .'</b></h3></div>'; 
     $mail->Body    = $bodyContent; 
